f3eb6aa6d0417358e96b695180294ee93f7ab5d3
vinay kumar <vk618464@gmail.com>
2026-03-12 20:45:40 +0530

    init

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    port: 5173,
+    port: 5174,
     proxy: {
       '/api': 'http://localhost:3001'
     }
